5c52fd24079264ddbd3d4dccbf668892f4707dfa
DevelopingWriter96 <staceyrichard1996@gmail.com>
2022-02-16 14:28:48 -0700

    More changes to api call

--- pages/utils/api-util.js
+++ pages/utils/api-util.js
@@ -1,5 +1,5 @@
 export async function getThemeData(theme) {
-    const response = await fetch(`http://brickset.com/api/v3.asmx/getSets?apiKey=3-kq43-jpaH-NZSZw&params={'theme':'${theme}'} HTTP/1.1`)
+    const response = await fetch(`http://brickset.com/api/v3.asmx/getSets?apiKey=3-kq43-jpaH-NZSZw&params={'theme':'${theme}'}`)
     const data = await response.json()
     console.log(data)
     return data
