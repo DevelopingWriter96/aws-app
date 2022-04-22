2e39e1ecaedc4d8781bfc5cfdc7e774923aa5cd5
DevelopingWriter96 <staceyrichard1996@gmail.com>
2022-04-22 16:33:36 -0600

    Route prototype

--- src/components/ResponsiveAppBar.js
+++ src/components/ResponsiveAppBar.js
@@ -13,7 +13,7 @@ import Tooltip from '@mui/material/Tooltip';
 import MenuItem from '@mui/material/MenuItem';
 import TextField from '@mui/material/TextField';
 import SearchIcon from '@mui/icons-material/Search';
-import { getSetData } from "../utils/api-util";
+import { getSetData } from "../pages/api/data";
 
 const pages = ['Sets', 'Themes', 'Years'];
 const settings = ['Profile', 'Account', 'Dashboard', 'Logout'];
--- src/pages/api/data.js
+++ src/pages/api/data.js
@@ -1,5 +1,20 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
 
-export default function handler(req, res) {
-  res.status(200).json({ name: 'John Doe' })
+export default async function handler(req, res) {
+  if (req.method === 'POST') {
+    const set = req.body.set
+
+  const response = await fetch("https://brickset.com/api/v3.asmx/getSets", {
+    method: 'POST',
+    body: new URLSearchParams({
+        'apiKey': '3-kq43-jpaH-NZSZw',
+        'params': JSON.stringify({setNumber: `${set}-1`}),
+        'userHash': ''
+    })
+  })
+  
+  const setData = await response.json()
+  
+  res.status(200).json(setData)
+  }
 }
